922e4ddeacb6dee7a6cfd9a7b6bc5dca89fd444d
Phil Long <phil.long.09@gmail.com>
2015-06-13 15:39:01 -0700

    catch all, redirect to index

--- wanderlust/views/__init__.py
+++ wanderlust/views/__init__.py
@@ -2,10 +2,6 @@ from flask import render_template, send_from_directory
 from wanderlust import app
 import config
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/static/<path:path>')
 def static_files(path):
     return send_from_directory('static', path)
@@ -14,5 +10,11 @@ def static_files(path):
 def ng_template(path):
     print path
     return render_template(path)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    print "attemping to load ", path
+    return render_template('index.html')
 
 ### PUT ROUTES HERE
